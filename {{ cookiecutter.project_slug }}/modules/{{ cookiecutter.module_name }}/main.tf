c9f0752f1e9489c8d6a30289a07e87747f13e803
nj.ara1995 <nj.ara1995@gmail.com>
2023-05-03 03:29:03 -0400

    fix: tflint issue fix

--- {{ cookiecutter.project_slug }}/modules/{{ cookiecutter.module_name }}/main.tf
+++ {{ cookiecutter.project_slug }}/modules/{{ cookiecutter.module_name }}/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    local = {
+      version = ">= 2.0.0"
+    }
+  }
+}
+
 resource "local_file" "my_file" {
   filename        = var.file_name
   content         = var.file_content
